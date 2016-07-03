d89248c056205ff2f6e6110cf9f3bdc63ed2afa6
Alexander Melihov <amelihovv@ya.ru>
2016-07-04 00:43:58 +0300

    fix styling

--- composer.sh
+++ composer.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
+
 echo "Current working directory: '"$(pwd)"'"
+
 docker run --rm -v $(pwd):/app -v ~/.ssh:/root/.ssh --link mysql:mysql \
     composer "$@"
+
