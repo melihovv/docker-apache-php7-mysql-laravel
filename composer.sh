1432f0ed9c4375af149a78ed928fae05cd7a9b9e
Alexander Melihov <amelihovv@ya.ru>
2016-07-20 03:09:12 +0300

    link to redis in php.sh and composer.sh

--- composer.sh
+++ composer.sh
@@ -5,5 +5,5 @@ export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 echo "Current working directory: '"$(pwd)"'"
 
 docker run --rm -v $(pwd):/app -v ~/.ssh:/root/.ssh --link mysql:mysql \
-    composer "$@"
+    --link laravel_redis:redis composer "$@"
 
--- php.sh
+++ php.sh
@@ -4,5 +4,6 @@ export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 
 echo "Current working directory: '"$(pwd)"'"
 
-docker run --rm -v $(pwd):/var/www/html --link mysql:mysql php "$@"
+docker run --rm -v $(pwd):/var/www/html --link mysql:mysql \
+    --link laravel_redis:redis php "$@"
 
